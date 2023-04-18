c0df43cd309d04cd60a6612a3054da182c1c797b
Gustavo <gustavosantos4991@gmail.com>
2023-04-18 20:52:11 -0300

    branch com o código da aula do dia Ter 18 Abr 2023 20:52:11 -03

---
+++ lib/data/prato_repository.dart
@@ -0,0 +1,10 @@
+import 'package:cardapio/domain/Prato.dart';
+
+List<Prato> loadPratos() {
+  return <Prato>[
+    Prato(
+      nome: "Sopa Verde", 
+      descricao: "Um monte de bagulho verde na sopa",
+    ),
+  ];
+}
---
+++ lib/domain/prato.dart
@@ -0,0 +1,9 @@
+class Prato {
+  final String nome;
+  final String descricao;
+
+  Prato({
+    required this.nome,
+    required this.descricao
+  });
+}
